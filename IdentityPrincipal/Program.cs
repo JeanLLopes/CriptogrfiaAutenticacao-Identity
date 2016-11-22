3dc4f219bbe64e969c643f3531056a957e56c5df
JeanLLopes <jeanllopes@outlook.com>
2016-11-22 20:23:30 -0200

    Initial commit

---
+++ IdentityPrincipal/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace IdentityPrincipal
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            //Todo os sistema funciona com os seguintes passos:
+
+            //1 - Preciso criar uma identidade
+
+            //2 - Para este fim preciso criar algumas declarações
+                    //Como nome, Telefone, papeis etc.
+                    //=> no mínimo devemos adicionar um nome
+
+            //3 - Juntar tudo e colocar no ambiente (Principal)
+
+            Console.ReadKey();
+        }
+    }
+}
